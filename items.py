c0890d0478edeae66b65c308029e3c609c69d6ec
unknown <dssanchez866@misena.edu.co>
2022-10-20 16:10:46 -0500

    Full

---
+++ items.py
@@ -0,0 +1,11 @@
+items= [
+    {
+        "name":"Guayos","price":50,"quantity":10
+    },
+    {
+        "name":"Camiseta","price":40,"quantity":10
+    },
+    {
+        "name":"Pantaloneta","price":30,"quantity":10
+    }
+]
